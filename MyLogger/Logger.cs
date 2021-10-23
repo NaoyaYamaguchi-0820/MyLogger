c0902c409dfcecb7e2bf8ba2462a66ae366d4976
NaoyaYamaguchi <naoya.yamaguchi.mail@gmail.com>
2021-10-23 14:38:54 +0900

    Create Logger & LoggerTest

---
+++ MyLogger/Logger.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLogger
+{
+    public class Logger
+    {
+        public void Info(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warn(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fatal(string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Debug(string message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyLoggerTest/LoggerTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MyLogger;
+
+namespace MyLoggerTest
+{
+    [TestClass]
+    public class LoggerTest
+    {
+        [TestMethod]
+        public void ViewTest()
+        {
+            Logger logger = new Logger();
+
+            logger.Info("TestMessage.");
+            logger.Warn("TestMessage.");
+            logger.Error("TestMessage.");
+            logger.Fatal("TestMessage.");
+            logger.Debug("TestMessage.");
+
+        }
+    }
+}
